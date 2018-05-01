e0cb053e8fc47db6ab5a0a020654fa9248b24e5c
Stephen Smith <workcpu@stephens-macbook-air.local>
2018-04-30 22:01:42 -0400

    Initial Commit

---
+++ ParaMeds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParaMeds
+//
+//  Created by Stephen Smith on 4/30/18.
+//  Copyright © 2018 Stephen Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
